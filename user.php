170009710879647ea3dc71b0289f6ae4594e757b
eddyenet <36435061+eddyenet@users.noreply.github.com>
2018-12-26 09:26:23 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini program ilegal".$t;
+echo $kuning."[-] resiko ditanggung penumpang!".$t.$ijo.$t;
+$sungging = $putih.'[*] '.$ijo.'subscribe kontol '.$putih.'[-] code: KONTOL';
+$ads = $putih."[x] official kontol";
+$msgtoto = "[!] subscribe KONTOL";
+$msgyg = "[!] server gangguan";
+$yd_stat = true;
+$vip1= false;
+$weblock =[""];
+$ydblock = [""];
+$we_stat = true;
+$msgwe = "[!] enjoy your life";
+$msgveeu = "[!] enjoy your life";
+
+$data = [];
+
+$toto = ['test'];
+$ydnews = ['tes','kintil'];
